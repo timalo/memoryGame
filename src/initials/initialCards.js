09b6e1e2aabd2b093cafaa6c5fd752994ea4809d
timalo <44393499+timalo@users.noreply.github.com>
2024-10-17 19:57:05 +0300

    Added some initialImages and Card components, will have to write CSS for them later.

---
+++ src/initials/initialCards.js
@@ -0,0 +1,14 @@
+const cards = [
+  {
+    id: 1,
+    title: "Card 1",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHv2Ctgb7u9zoOdSRNY7co44Tr97wQlL5vBSszxE8CCcfoPnQ3qoNxFfXllUWOskF4pB0&usqp=CAU",
+  },
+  {
+    id: 2,
+    title: "Card 2",
+    imageUrl: "https://www.w3schools.com/w3images/lights.jpg",
+  },
+];
+export default cards;
