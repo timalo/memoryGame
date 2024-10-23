821aa6fb460e80003f5494e9861a9fab3a2f2e4e
timalo <44393499+timalo@users.noreply.github.com>
2024-10-24 02:35:10 +0300

    Small chhanges to card component.

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,6 @@
 export default function Card(props) {
-  function cardClick(e) {
-    //Handle card click and everything related to it
-    console.log("Card clicked");
-    console.log(e.target);
-  }
-
   return (
-    <div className="cardDiv" onClick={cardClick}>
+    <div className="cardDiv" onClick={props.cardClick}>
       <div className="cardImageDiv">
         <img className="cardImage" src={props.imageUrl}></img>
       </div>
--- src/Game.jsx
+++ src/Game.jsx
@@ -5,16 +5,46 @@ export default function Game(props) {
   const [shuffledCards, setShuffledCards] = useState([]);
   const [currentScore, setCurrentScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [clickedCards, setClickedCards] = useState([]);
 
   useEffect(() => {
-    console.log("Game component mounted");
+    shuffleCards();
+  }, [props.currentFetch]);
+
+  function shuffleCards() {
     const randomCards = props.currentFetch
       .sort(() => Math.random() - 0.5)
       .slice(0, 10);
     setShuffledCards(randomCards);
-  }, [props.currentFetch]);
+  }
 
+  function cardClick(e) {
+    console.log("Card clicked");
+    console.log(e);
+    console.log(e.target);
+  }
+  /* 
+  function checkCardClicked(cardId) {
+    if (clickedCards.includes(cardId)) {
+      setCurrentScore(0);
+      setClickedCards([]);
+    } else {
+      const newClickedCards = [...clickedCards, cardId];
+      setClickedCards(newClickedCards);
+      setCurrentScore(currentScore + 1);
+      if (currentScore >= highScore) {
+        setHighScore(currentScore + 1);
+      }
+    }
+  } */
+
+  //TODO: implement the cardClick function, will have to find a way to associate certain card with certain id.
   return shuffledCards.map((card, index) => (
-    <Card key={index} imageUrl={card.imageUrl} title={card.title} />
+    <Card
+      key={index}
+      imageUrl={card.imageUrl}
+      title={card.title}
+      cardClick={cardClick}
+    />
   ));
 }
